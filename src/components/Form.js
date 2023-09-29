bb982c5aaa9b8f257f78aef0321038d8ea29db65
vaibhavw12 <vaibhavwable2000@gmail.com>
2023-09-29 11:00:16 +0530

    added card details page with validation

---
+++ src/components/Form.js
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import './Form.css';
+
+function Form() {
+
+    let [name, setName] = useState('')
+    let [number, setNumber] = useState('')
+    let [month, setMonth] = useState('')
+    let [year, setYear] = useState('')
+    let [cvv, setCvv] = useState('')
+    let [errorTextNumber, setErrorTextNumber] = useState('')
+    let [errorTextName, setErrorTextName] = useState('')
+    let [errorTextCvv, setErrorTextCvv] = useState('')
+    let [errorTextDate, setErrorTextDate] = useState('')
+
+    function submit(e){
+
+        e.preventDefault()
+
+        // console.log(name)
+        // console.log(number)
+        // console.log(month)
+        // console.log(year)
+        // console.log(cvv)
+
+        if(validation()){
+            return
+        }
+
+        setErrorTextName('')
+        setErrorTextNumber('')
+        setErrorTextCvv('')
+        setErrorTextDate('')
+        setName('')
+        setNumber('')
+        setMonth('')
+        setYear('')
+        setCvv('')
+
+    }
+
+    function validation(){
+        if(name.length < 1){
+            setErrorTextName('enter a valid card name')
+            setErrorTextNumber('')
+            setErrorTextCvv('')
+            setErrorTextDate('')
+            return true
+        }if(number.length !== 16){
+            setErrorTextNumber('enter a valid card number')
+            setErrorTextName('')
+            setErrorTextCvv('')
+            setErrorTextDate('')
+            return true
+        }if(cvv.length !== 3){
+            setErrorTextCvv('enter a valid card cvv')
+            setErrorTextName('')
+            setErrorTextNumber('')
+            setErrorTextDate('')
+            return true
+        }if(month.length !== 2 || year.length !== 2 || parseInt(month) > 12){
+            setErrorTextDate('enter a valid card date')
+            setErrorTextName('')
+            setErrorTextNumber('')
+            setErrorTextCvv('')
+            return true
+        }
+        return false
+    }
+
+  return (
+    <div className='page'>
+        <div className='leftPage'>
+            hello
+        </div>
+        <div className='rightPage'>
+            <form className='form' onSubmit={submit}>
+                <section>
+                    <label>
+                        Cardholder Name <br></br>
+                        <input id='name' placeholder='e.g. Jane Appleseed' type='text'
+                        value={name} onChange={(e) =>{
+                            setName(e.target.value)
+                        }} ></input>
+                         <p className='error'>{errorTextName}</p>
+                        {/* <br></br> <br></br> */}
+                    </label>
+                </section>
+                    <label>
+                        Card Number <br></br>
+                        <input id='number' placeholder='e.g. 1234 5678 9123 0000' type='number'
+                        value={number} onChange={(e) =>{
+                            setNumber(e.target.value)
+                        }} ></input>
+                        <p className='error'>{errorTextNumber}</p>
+                        {/* <br></br> <br></br> */}
+                    </label>
+                <section className='both'>
+                    <label className='date'>
+                        Exp.Date(MM/YY)   <br></br>
+                        <input className='date-in' placeholder='MM' type='text'
+                        value={month} onChange={(e) =>{
+                            setMonth(e.target.value)
+                        }} >
+                            </input> <input className='date-in' placeholder='YY' type='text' 
+                            value={year} onChange={(e) =>{
+                                setYear(e.target.value)
+                            }} ></input> 
+                            <p className='error'>{errorTextDate}</p>
+                    </label>
+                    <label className='cvv'>
+                        Cvv<br></br>
+                        <input className='cvv-in'  placeholder='e.g. 123' type='number' 
+                        value={cvv} onChange={(e) =>{
+                            setCvv(e.target.value)
+                        }} ></input>
+                         <p className='error'>{errorTextCvv}</p>
+                        {/* <br></br>  <br></br> */}
+                    </label>
+                </section>
+                <section>
+                    <button>Confirm</button>
+                </section>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Form
